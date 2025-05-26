815f75bb76f5b7ce6e0ec898e699e4bfb67d3244
Garima Barthwal <garima.barthwal@newgensoft.com>
2025-05-26 15:44:48 +0530

    Day 1: Initial API project setup with HelloController and Swagger

---
+++ SmartInventory.API/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartInventory.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult SayHello()
+        {
+            return Ok("Welcome to SmartInventorySystem API!");
+        }
+    }
+}
---
+++ SmartInventory.API/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "SmartInventorySystem API",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "SmartInventorySystem API v1");
+    });
+}
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
